8ddd727a89b9d3f32f3f5ff4137a7616b5b986d3
RezzAja <f4khrurrozy.22nkh@gmail.com>
2025-10-15 11:49:58 +0700

    SiakadWhile

---
+++ SiakadWhile14.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SiakadWhile14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, i = 0;
+        double nilai;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        n = sc.nextInt();
+
+        while (i < n) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai = sc.nextDouble();
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid!");
+                continue;
+            }
+
+            if (nilai >= 85 && nilai <= 100) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+            } else if (nilai >= 75) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B");
+            } else if (nilai >= 60) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
+            } else if (nilai >= 50) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
+            } else {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
+            }
+            i++;
+        }
+    }
+}
